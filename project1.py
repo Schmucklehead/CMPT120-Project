240db6f5faaf09c0d1fcf7d72bea57ac34db4409
jaketantorski1 <jake.tantorski1@marist.edu>
2017-09-12 23:07:36 -0400

    Project

---
+++ project1.py
@@ -0,0 +1,54 @@
+
+score = 0
+
+slime = ("You have enter the slime realm. Every inch of the planet is covered in goo and it is near impossible to moved around. You glanced a see a trap door and shimmy down it before you get stuck")
+dark =("After successfully navigating out of the slime realm you find yourslef in a room with only one candle light. You hear creeks and moans and run toward the candle. You see a button and press it. Suddnely light")
+zi = ("You appeared from the light into a city with flying cars and millions of people in tubes traveling in all diferent directions. A sign reads Zifler: PLanet of Glass. Before you can even think a flying saucer comes and zaps you up.")
+ship = ("Aboard the ship you are greeted by aliens you have nevr seen. They asked you where you want to go. Baffled by the question you respond with the one place you have been trying to find pure happiness: The Daytona 500")
+race = ("You have finally arrived at the Daytona 500. The one place you can find happiness. You are at peace.")
+
+
+print("Welcome to The Pursuit of Happiness")
+print("On this adventure you will enter into many different realms.All youve ever wanted was to see was the Daytona 500 but you have awoken in the strangest place")
+print ("Your score is: "  + str(score) + "  -Press Enter")
+print()
+
+
+
+input()
+myLocation = slime
+print(myLocation)
+score = score + 5
+print ("Your score is: " + str(score)+ "  -Press Enter")
+print()
+
+input()
+myLocation = dark
+print(myLocation)
+score = score + 5
+print ("Your score is: " + str(score)+ "  -Press Enter")
+print()
+
+input()
+myLocation = zi
+print(myLocation)
+score = score + 5
+print ("Your score is: " + str(score)+ "  -Press Enter")
+print()
+
+input()
+myLocation = ship
+print(myLocation)
+score = score + 5
+print ("Your score is: " + str(score)+ "  -Press Enter")
+print()
+       
+input()
+myLocation = race
+print(myLocation)
+print("You think back on the day youve had. You couldve never imagined seeing these places and having these things happen to you but you are thankful you get to watch the best race on earth.")
+score = score + 5
+print ("Your score is: " + str(score)+ "  -Press Enter")
+print("Jake Tantorski jake.tantorski1@gmail.com")
+
+
