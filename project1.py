ba5e9aeac60a6f431619d8e11252ec8a75acf0ca
jaketantorski1 <jake.tantorski1@marst.edu>
2017-09-14 23:21:05 -0400

    grammar

--- project1.py
+++ project1.py
@@ -1,10 +1,9 @@
 
 score = 0
-
-slime = ("You have enter the slime realm. Every inch of the planet is covered in goo and it is near impossible to moved around. You glanced a see a trap door and shimmy down it before you get stuck")
-dark =("After successfully navigating out of the slime realm you find yourslef in a room with only one candle light. You hear creeks and moans and run toward the candle. You see a button and press it. Suddnely light")
-zi = ("You appeared from the light into a city with flying cars and millions of people in tubes traveling in all diferent directions. A sign reads Zifler: PLanet of Glass. Before you can even think a flying saucer comes and zaps you up.")
-ship = ("Aboard the ship you are greeted by aliens you have nevr seen. They asked you where you want to go. Baffled by the question you respond with the one place you have been trying to find pure happiness: The Daytona 500")
+slime = ("You have entered the slime realm. Every inch of the planet is covered in goo and it is nearly impossible to move around. You glance and see a trap door; shimmying down it before you get stuck")
+dark =("After successfully navigating out of the slime realm, you find yourself in a room lit by only one candle. You hear creaks and moans;running toward the candle. You see a button and press it. Suddenly, light")
+zi = ("You appear from the light in a city with flying cars and millions of people in tubes traveling in all different directions. A sign reads Zifler: PLanet of Glass. Before you can even think, a flying saucer comes and zaps you up.")
+ship = ("Aboard the ship you are greeted by aliens you have never seen. They ask you where you want to go. Baffled by the question you respond with the one place you have been trying to find, pure happiness: The Daytona 500")
 race = ("You have finally arrived at the Daytona 500. The one place you can find happiness. You are at peace.")
 
 
@@ -42,13 +41,14 @@ print(myLocation)
 score = score + 5
 print ("Your score is: " + str(score)+ "  -Press Enter")
 print()
+
        
 input()
 myLocation = race
 print(myLocation)
-print("You think back on the day youve had. You couldve never imagined seeing these places and having these things happen to you but you are thankful you get to watch the best race on earth.")
+print("You think back on the day you've had. You could've never imagined seeing these places and having these things happen to you but you are thankful you get to watch the best race on earth.")
 score = score + 5
 print ("Your score is: " + str(score)+ "  -Press Enter")
-print("Jake Tantorski jake.tantorski1@gmail.com")
+print("Copyright: Jake Tantorski jake.tantorski1@gmail.com")
 
 
