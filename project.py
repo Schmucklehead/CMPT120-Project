be4fcde21ecea516fc3a8b9180b08f0f05e700e2
jaketantorski1 <jake.tantorski1@marist.edu>
2017-09-26 19:39:48 -0400

    Username

--- project.py
+++ project.py
@@ -1,49 +1,56 @@
 #Jake Tantorski CMPT 120L 
 
-name = input(str("Enter your name: "))
+
 score = 0
 
 
-slime = ("You have entered the slime realm. Every inch of the planet is covered in goo and it is nearly impossible to move around. You glance and see a trap door; shimmying down it before you get stuck")
-dark =("After successfully navigating out of the slime realm, you find yourself in a room lit by only one candle. You hear creaks and moans;running toward the candle. You see a button and press it. Suddenly, light")
-zi = ("You appear from the light in a city with flying cars and millions of people in tubes traveling in all different directions. A sign reads Zifler: PLanet of Glass. Before you can even think, a flying saucer comes and zaps you up.")
-ship = ("Aboard the ship you are greeted by aliens you have never seen. They ask you where you want to go. Baffled by the question you respond with the one place you have been trying to find, pure happiness: The Daytona 500")
-meteor = ("As the alien spaceship flies through the sky another ship fles behind it, shooting. To avoid the lasers the ship heads into and meteor and into a deep crater.")
-worm = ("After narrowly escaping the other ship the captain realizes something isnt right. He quickly grabs the contorls to fly out of the crater when all of a sudden the crater starts to close. we whiz pass the teeth of what looked to be a space worm and we made it out in the nick of time.")
-race = ("You have finally arrived at the Daytona 500. The one place you can find happiness. You are at peace.")
+rocks = ("You stumble upon a rocky surface. The is no life to be seen and water is scarce")
+beach = ("A beach appears. Waves crash against the sandy beach and palm trees sway in the wind")
+cave = ("Between sime bushes a cave is visible. You walk inside and see many drawings on the wall and a torch lit in the back")
+forest = ("A thick forest appears with many tall luming trees. Aniamls are abdunant and you smell pine")
+field = ("Now a lush grassy field is in your sights. The grass is untouched except the small rodents that live in it. Flies buzz around your head in the heat")
+village = ("You can make out what seems to be an old village. A fire is almost out and spears are lying around")
 
 
-print("Welcome to The Pursuit of Happiness")
-print("On this adventure you will enter into many different realms.All youve ever wanted was to see was the Daytona 500 but you have awoken in the strangest place")
-print (name + "'s score is: "  + str(score) + "  -Press Enter")
-print()
+title = ("Welcome to The Pursuit of Happiness. On this adventure you will enter into many locations. Hopefully you can make it out alive")
+def intro():
+    print(title)
+    intro.name = input(str("Enter your name: "))
+intro()
             
 
 
 
 input()
-myLocation = slime
+myLocation = rocks
 print(myLocation)
 score = score + 5
 print (name + "'s score is: "  + str(score) + "  -Press Enter")
 print()
 
 input()
-myLocation = dark
+myLocation = beach
 print(myLocation)
 score = score + 5
 print (name + "'s score is: "  + str(score) + "  -Press Enter")
 print()
 
 input()
-myLocation = zi
+myLocation = cave
 print(myLocation)
 score = score + 5
 print (name + "'s score is: "  + str(score) + "  -Press Enter")
 print()
 
 input()
-myLocation = ship
+myLocation = forest
+print(myLocation)
+score = score + 5
+print (name + "'s score is: "  + str(score) + "  -Press Enter")
+print()
+           
+input()
+myLocation = village
 print(myLocation)
 score = score + 5
 print (name + "'s score is: "  + str(score) + "  -Press Enter")
@@ -51,9 +58,9 @@ print()
 
        
 input()
-myLocation = race
+myLocation = field
 print(myLocation)
-print("You think back on the day you've had. You could've never imagined seeing these places and having these things happen to you but you are thankful you get to watch the best race on earth.")
+print("Youve been everywhereit seems like. You think back on the day you've had. You could've never imagined seeing these places and having these things happen to you but you are thankful you get to see another day on the island.")
 score = score + 5
 print (name + "'s score is: "  + str(score) + "  -Press Enter")
 print("Copyright: Jake Tantorski jake.tantorski1@gmail.com")
